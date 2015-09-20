188f53eb57e9b33b1473a5d695c539c47e3bb6a3
kirbycm <kirbycm@gmail.com>
2015-09-20 10:46:28 -0400

    Long description fix.

--- hide_code/hide_code.py
+++ hide_code/hide_code.py
@@ -8,7 +8,7 @@ def __main__():
 
 
 def install(nb_path=None, DEBUG=False):
-	print 'starting install'
+	print 'Starting hide_code.js install'
 	current_dir = path.abspath(path.dirname(__file__))
 	site_packages_path = os.__file__[:-6] + "site-packages"
 
@@ -26,10 +26,14 @@ def install(nb_path=None, DEBUG=False):
 	# copy js into static/custom directory in Jupyter/iPython directory
 	if(path.isdir(install_path)):
 		shutil.copyfile(current_dir + "/hide_code.js", install_path + "/hide_code.js")
-		print 'Copying hide_code.js to notebook/static/custom/'
+		print 'Copying hide_code.js to ' + install_path 
 
 		# add require to end of custom.js to auto-load on notebook startup
 		with open(install_path + "/custom.js", 'a') as customJS:
 			with open(current_dir + "/auto-load.txt") as auto:
 				customJS.write(auto.read())
 				print "Configured custom.js to auto-load hide_code.js"
+	else:
+		print 'Unable to install into ' + install_path
+		print 'Directory doesn\' exist.'
+		print 'Make sure Jupyter is installed.'
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class install(_install):
 
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -31,7 +31,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description='A Jupyter notebook extension to hide code.',
     long_description=long_description,
@@ -78,7 +78,7 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    packages={'','hide_code'},
+    packages={'hide_code'},
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
