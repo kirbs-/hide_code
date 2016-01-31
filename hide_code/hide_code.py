4cac7adba897f11530d4ec7ca486fec94bc2a816
kirbycm <kirbycm@gmail.com>
2016-01-30 21:05:23 -0500

    Fixed markdown cell issue.

--- hide_code/hide_code.py
+++ hide_code/hide_code.py
@@ -68,7 +68,7 @@ def install(nb_path=None, DEBUG=False):
 
 def get_site_package_dir():
 	os_file = os.__file__
-	if os_file.endswith('c'):
+	if os_file.endswith('.pyc'):
 		return path.join(os_file[:-7], "site-packages")
 	else:
 		return path.join(os_file[:-6], "site-packages")
