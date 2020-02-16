0492d2653c80b4c89a235c41256c87c236f61d0b
Chris Kirby <chris.kirby@asurion.com>
2020-02-16 14:01:26 -0500

    Updated Latex PDF exporting. Resolves #65.

---
+++ hide_code/hide_code_latexpdf_exporter.py
@@ -0,0 +1,25 @@
+import os
+import os.path
+
+from traitlets.config import Config
+from nbconvert.exporters.pdf import PDFExporter
+from traitlets.log import get_logger
+
+
+class HideCodeLatexPDFExporter(PDFExporter):
+    def __init__(self, config=None, **kw):
+        # self.register_preprocessor('hide_code.HideCodePreprocessor', True)
+        super(HideCodeLatexPDFExporter, self).__init__(config, **kw)
+        self.preprocessors = ['hide_code.HideCodePreprocessor']
+        self._init_preprocessors()
+
+    def _template_file_default(self):
+        return 'hide_code_article'
+
+    @property
+    def template_path(self):
+        """
+        We want to inherit from HTML template, and have template under
+        `./templates/` so append it to the search path. (see next section)
+        """
+        return super(HideCodeLatexPDFExporter, self).template_path + [os.path.join(os.path.dirname(__file__), "Templates")]
--- hide_code/hide_code_pdf_exporter.py
+++ hide_code/hide_code_pdf_exporter.py
@@ -1,20 +1,36 @@
 import os
 import os.path
+import pdfkit
+
+
+# from hide_code.hide_code_html_exporter import HideCodeHTMLExporter
+
 
 from traitlets.config import Config
-from nbconvert.exporters.pdf import PDFExporter
+from traitlets import default
+# from nbconvert.exporters.pdf import PDFExporter
+from nbconvert.exporters.html import HTMLExporter
 from traitlets.log import get_logger
 
 
-class HideCodePDFExporter(PDFExporter):
+class HideCodePDFExporter(HTMLExporter):
     def __init__(self, config=None, **kw):
         # self.register_preprocessor('hide_code.HideCodePreprocessor', True)
         super(HideCodePDFExporter, self).__init__(config, **kw)
-        self.preprocessors = ['hide_code.HideCodePreprocessor']
-        self._init_preprocessors()
+        # self.preprocessors = ['hide_code.HideCodePreprocessor']
+        # self._init_preprocessors()
+
+    @default('file_extension')
+    def _file_extension_default(self):
+        return '.pdf'
+
+    def from_notebook_node(self, nb, resources=None, **kw):
+        output, resources = super(HideCodePDFExporter, self).from_notebook_node(nb, resources, **kw)
+        output = pdfkit.from_string(output, False)
+        return output, resources
 
     def _template_file_default(self):
-        return 'hide_code_article'
+        return 'hide_code_full.tpl'
 
     @property
     def template_path(self):
